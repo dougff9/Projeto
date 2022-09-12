672a9ebbf53ca6e4eb6c0ed3eabe367a9b75b7c2
dougff9 <dougff9@live.com>
2022-09-12 10:04:47 -0300

    Conhecendo Classes e Objetos

--- Program.cs
+++ Program.cs
@@ -1,8 +1,41 @@
 ﻿using Projeto_1.Models;
 
-Pessoa pessoa1 = new Pessoa();
+DateTime dataAtual = DateTime.Now.AddDays(4);
+Console.WriteLine (dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
-pessoa1.Nome = "Douglas";
-pessoa1.Idade = 34;
-pessoa1.Apresentar();
+
+
+
+
+
+
+// string apresentacao = "Olá, seja bem vindo";
+
+
+
+// int quantidade = 1;
+// Console.WriteLine("Valor da variável quantidade " + quantidade);
+
+// quantidade = 10;
+// Console.WriteLine("Valor da variável quantidade " + quantidade);
+
+// double altura = 1.80;
+
+// decimal preco = 1.75M;
+
+// bool condicao = true;
+
+//Console.WriteLine(apresentacao);
+//Console.WriteLine("Valor da variável qunatidade: " + quantidade);
+//Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+//Console.WriteLine("Valor da variável preco: R$" + preco);
+//Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+
+//Pessoa pessoa1 = new Pessoa();
+
+//pessoa1.Nome = "Douglas";
+//pessoa1.Idade = 34;
+//pessoa1.Apresentar();
 
